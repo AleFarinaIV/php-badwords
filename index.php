b33ce8ac1741ce0151baf19320fb1eb3c492d108
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-04 15:24:52 +0200

    [UPD] setted form

--- index.php
+++ index.php
@@ -10,7 +10,27 @@
 </head>
 
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 p-5">
+                <form action="" method="GET">
+                    <div class="w-50">
+                        <label for="paragraph">Scrivi qui il tuo paragrafo</label>
+                        <textarea class="form-control" name="paragrafo" id="paragraph" placeholder="Scrivi..."></textarea>
+                    </div>
 
+                    <div class="form-group mt-5">
+                        <div class="w-25">
+                            <label for="text">Scrivi qui la parola da censurare</label>
+                            <input class="form-control" type="text" name="badworse" id="text" placeholder="Censura">
+                            <button class="form-control bg-primary mt-3" type="submit">Submit</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
