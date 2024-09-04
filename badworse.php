56494bede6faf81ff8a014e36fa9b1cb829ae8a7
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-04 17:43:42 +0200

    [FIX] fixed errors

--- badworse.php
+++ badworse.php
@@ -24,33 +24,33 @@
 
     };
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Badworse</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>Badworse</title>
+    </head>
 
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 p-5">
-                <div class="mb-5">
-                    <h1>
-                        Questo è il tuo testo censurato
-                    </h1>
-                    <p class="fs-4 fst-italic"> "<?php echo $censored_paragraph ?>" </p>
-                    <h6>Il tuo testo è lungo: <?php echo $lenght ?> caratteri</h6>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 p-5">
+                    <div class="mb-5">
+                        <h1>
+                            Questo è il tuo testo censurato
+                        </h1>
+                        <p class="fs-4 fst-italic"> "<?php echo $censored_paragraph ?>" </p>
+                        <h6>Il tuo testo è lungo: <?php echo $lenght ?> caratteri</h6>
+                    </div>
+                    <h3>Questa invece è la parola che hai censurato: '<span class="text-danger fst-italic"><?php echo $badworse ?></span>'</h3>
                 </div>
-                <h3>Questa invece è la parola che hai censurato: '<span class="text-danger fst-italic"><?php echo $badworse ?></span>'</h3>
             </div>
-    </div>
+        </div>
 
-</body>
+    </body>
 
 </html>
